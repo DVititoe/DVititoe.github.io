09734c9f9946997343fb8e2dba60d8008af1bfbd
DVititoe <DavidVititoe70@gmail.com>
2025-04-09 21:07:34 -0400

    Documentaton Update

---
+++ Solo_Controller/html/search/all_1c.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['_7ebutton_0',['~Button',['../class_button.html#aec43891dc8ff8f87e0eff95f8315f09f',1,'Button']]],
+  ['_7ecanvas_1',['~Canvas',['../class_canvas.html#a11136afee83f7bfa3d8f16d51abe9a70',1,'Canvas']]],
+  ['_7edram_5fcanvas_2',['~DRAM_Canvas',['../class_d_r_a_m___canvas.html#a8a44d287d9bf8ea0c8268be5a7fc4436',1,'DRAM_Canvas']]],
+  ['_7eimmediatebutton_3',['~ImmediateButton',['../class_immediate_button.html#aa9b3f0e0f1b913650fdf244d1a731044',1,'ImmediateButton']]],
+  ['_7emomentarybutton_4',['~MomentaryButton',['../class_momentary_button.html#a2b6d4300540557b8574639902d7e204f',1,'MomentaryButton']]],
+  ['_7erom_5fcanvas_5',['~ROM_Canvas',['../class_r_o_m___canvas.html#a12f9d19af9b2175a768475b348197e26',1,'ROM_Canvas']]],
+  ['_7esensor_6',['~Sensor',['../class_sensor.html#a2561088ed8350df5d5d53a079b57a113',1,'Sensor']]],
+  ['_7esolomenu_7',['~SoloMenu',['../class_solo_menu.html#a1303f286bd0f1073039c252d3cdbe637',1,'SoloMenu']]],
+  ['_7etogglebutton_8',['~ToggleButton',['../class_toggle_button.html#a3f31eac6d75ced6149139d37a7024170',1,'ToggleButton']]]
+];
---
+++ Solo_Controller/html/search/pages_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['touch_20screen_20biab_20controller_0',['Touch Screen BIAB Controller',['../index.html',1,'']]]
+];
