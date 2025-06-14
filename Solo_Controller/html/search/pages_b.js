4c382b9f4056a3e3dfded95bedd1838a66083bb7
DVititoe <DavidVititoe70@gmail.com>
2025-06-13 20:57:51 -0400

    Updated docs

---
+++ Solo_Controller/html/search/pages_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['software_20architecture_0',['Software Architecture',['../_software__architecture.html',1,'index']]],
+  ['started_1',['Getting Started',['../_getting__started.html',1,'index']]]
+];
